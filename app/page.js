9d5fdb1d2202f47781d9e74295faa49f8506012b
mohit yadav <mohitnandvanshi3@gmail.com>
2023-10-17 21:20:39 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,198 @@
-import Image from 'next/image'
+import React from "react"
+import Image from "next/image"
+export default function Home(){
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  return(
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+<div className="main"
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+>
+
+
+  <div className="dashbord">
+<h1>Dashbord</h1>
+<li className="md">Dashboard</li>
+ <ul>
+ <li>Product  <span>&gt;</span></li>
+ <li>Customer<span>&gt;</span></li>
+<li>Income  <span>&gt;</span></li>
+<li>Pramote  <span>&gt;</span></li>
+
+<li>  Help           <span>&gt;</span> </li>
+ </ul>
+ <div className="ld">
+<Image className="mt-35 rounded-full" src="/nina.png" width={50} height={50}/>
+<h2 className="my-59">Evano</h2>
+<p>Project manager</p>
+<span></span>
+
+
+ </div>
+ 
+  </div>
+
+  <div className="rdiv">
+
+  <div className="up">
+  <h2>Hello Shahruk &#128075; </h2>
+
+</div>
+<div className="inp">
+<input className="yin"  type="text" placeholder="Search"></input>
+</div>
+
+<div className="main2">
+ <div className="Earn">
+  <img src=""></img>
+
+    <p>Earning</p>
+    <h1>$198k</h1>
+    <p><span>37.8% this month</span>
+  </p>
+</div>
+<div className="Earn">
+  <img src=""></img>
+
+    <p>orders</p>
+    <h1>$2.4k</h1>
+    <p><span>2% this month</span>
+  </p>
+</div><div className="Earn">
+  <img src=""></img>
+
+
+    <p>balance</p>
+    <h1>$2.4k</h1>
+    <p><span>2% this month</span>
+  </p>
+</div><div className="Earn">
+  <img src=""></img>
+
+ 
+    <p>Total </p>
+    <h1>$198k</h1>
+    <p><span>37.8% this month</span>
+  </p>
+</div></div>
+
+
+
+
+  </div>
+  <div className="m3">
+
+
+<div className="left">
+<div className="eee">
+<h1>Overview </h1>
+<p>MOnthly Earning</p>
+
+<p>Quaterterly ^</p>
+
+</div>
+<span> jan</span>
+<span>Feb</span><span>march</span><span>april</span><span>may</span><span>jun</span><span>july</span><span>aug</span><span>sept</span><span>Oct</span>
+<span>Nov</span><span>Des</span>
+<div className="sp">
+
+</div>
+<div className="ri">
+<div className="sp">
+
+</div>
+<div className="sp">
+
+</div><div className="sp">
+
+</div><div className="sp">
+
+</div><div className="sp">
+
+</div><div className="op bg-purple-700">
+
+</div><div className="sp">
+
+</div><div className="sp">
+
+</div><div className="sp">
+
+</div><div className="sp">
+
+</div>
+</div>
+</div>
+
+<div className="right">
+
+<h1>Customers</h1>
+<p>Customers that buy products</p>
+<div className="gola">
+<div className="pgola">
+<div className="cgola">
+  <h1>65%</h1>
+  <p>Total New Customer</p>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+<div className="last">
+
+<div className="ll">
+
+<h1 className="font-bold">Product sell</h1>
+<p>Product Name</p>
+<div className="uuuu">
+<Image className="yyy mt-35 rounded-full" src="/Project1.png" width={50} height={50}/>
+<h1 className="font-bold">Abstract 3d</h1>
+
+<p>lorem hjg vj hh  h  h k hkhk</p>
+
+
+
+<Image className="iii mt-35 rounded-full" src="/project2.png" width={50} height={50}/>
+<h1 className="font-bold">Abstract 3d</h1>
+<p>lorem hjg vj hh  h  h k hkhk</p>
+</div>
+</div>
+<div className="lr">
+
+
+  <div className="1">
+    <input placeholder="search"></input>
+<p>stocks</p>
+<h5>32 in stock</h5>
+<h5>32 in stock</h5>
+
+  </div>
+  <div className="m22">
+  
+<p>price</p>
+<h5 className="font-bold"> $ 45.00 </h5>
+<h5 className="font-bold"> $  45.99 </h5>
+
+  </div>
+  <div className="1">
+  <span>Last 30 days</span>
+<p>Total sales</p>
+<h5>20</h5>
+<h5>20</h5>
+
+  </div>
+  
+
+  
+</div>
+
+</div>
+</div>
+    </>
   )
 }
